303dfb2ff37168fc668a9de9fed9d1a619f2bbe7
Katie Taylor <katiemxtaylor@gmail.com>
2018-08-14 14:53:49 -0700

    first push

--- createAccount.js
+++ createAccount.js
@@ -1,5 +1,8 @@
-var userDict = [];
-function createAccount(username, password, email) { //submit the variables and change the screen to the home screen
-
-  
-}
+var userDict = {};
+function createAccount(username, pwd, email) { //submit the variables and change the screen to the home screen
+  userInfoList = [];
+// push = append
+  userInfoList.push(pwd, email);
+  userDict["username"] = userInfoList
+  console.log(username);
+};
---
+++ signIn.js
@@ -0,0 +1,18 @@
+function signInCheck(username_entered, password_entered){
+  //dict["key"] --> key[list] (in list, 0 = pwd and 1 = email)
+
+  //sign in test w/ info:
+  userInfoList = [];
+  userInfoList.push("b", "c@c.com");
+  username = "a"
+  userDict["username"] = userInfoList
+
+  if (password_entered == userDict["username_entered"][0]) {
+    // need to send user to home screen
+    console.log(userDict["username_entered"][0])
+    alert(correct)
+  }
+  // else {
+  //   alert(Your username/password was not correct.);
+  // };
+};
