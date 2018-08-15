af5b83deb33c85207c4281ecc095f3a055158292
Katie Taylor <katiemxtaylor@gmail.com>
2018-08-15 12:36:57 -0700

    update to new respository

--- createAccount.js
+++ createAccount.js
@@ -1,8 +1,12 @@
-var userDict = {};
+var usersArr = []; //NEED to load users to a file and read it back in each time page loaded
+
 function createAccount(username, pwd, email) { //submit the variables and change the screen to the home screen
-  userInfoList = [];
-// push = append
-  userInfoList.push(pwd, email);
-  userDict["username"] = userInfoList
-  console.log(username);
+  var userObj = {}; //defines a new user object and adds info to user array; users numbered by their index in the list based on when they were added
+  userObj.username = username;
+  userObj.password = pwd;
+  userObj.emailAddress = email;
+  usersArr.push(userObj);
+//as a check: alert prints the username value stored in the user object in the first index of the array
+//how do I reference the username property's value in the first object (in the array)?
+  alert(usersArr[0].username);
 };
--- signIn.js
+++ signIn.js
@@ -2,15 +2,18 @@ function signInCheck(username_entered, password_entered){
   //dict["key"] --> key[list] (in list, 0 = pwd and 1 = email)
 
   //sign in test w/ info:
-  userInfoList = [];
-  userInfoList.push("b", "c@c.com");
-  username = "a"
-  userDict["username"] = userInfoList
+  var usersArr = [];
+  var userObj = {};
+  userObj.username = username;
+  userObj.password = pwd;
+  userObj.emailAddress = email;
+  usersArr.push(userObj);
 
-  if (password_entered == userDict["username_entered"][0]) {
+  //checks if the password and username entered math with a user in the array
+  if (username_entered in usersArr[0]) {
+    if (password_entered in usersArr[0])
+      alert(correct)
     // need to send user to home screen
-    console.log(userDict["username_entered"][0])
-    alert(correct)
   }
   // else {
   //   alert(Your username/password was not correct.);
