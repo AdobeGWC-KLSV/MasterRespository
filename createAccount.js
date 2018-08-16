acf17873f6356842be3ef9893a08d012bf32600a
Katie Taylor <katiemxtaylor@gmail.com>
2018-08-16 11:14:29 -0700

    update to new respository

--- createAccount.js
+++ createAccount.js
@@ -1,11 +1,32 @@
-var usersArr = []; //NEED to load users to a file and read it back in each time page loaded
+//load fs module to use Node.js for reading/writing to json files
+const fs = require("fs");
+////read json file strings into a single object
+//var usersText = fs.readFile("users.json");
+//var oldUsersArrObj = JSON.parse(usersText);
+////turns usersArr object into array of user objects (the keys are duplicates of the values, so we don't need them)
+//const usersArr = Object.values(oldUsersArrObj);
 
- //defines a new user object and adds info to user array; users numbered by their index in the list based on when they were added
+//defines a new user object and adds info to user array; users numbered by their index in the list based on when they were added
+var usersArr = [] //empty array for initial test before trying to read the JSON file
 function createAccount() {
   var userObj = {};
   userObj.username = document.getElementById('newUsernameBox').value; //adding the inputted value based on the element ID
   userObj.password = document.getElementById('newPwdBox').value;
   userObj.emailAddress = document.getElementById('newEmailBox').value;
   usersArr.push(userObj);
+  //convert usersArr to newUsersArrObj
+  var newUsersArrObj = {};
+  for (var i=0; i<usersArr.length; i++) {
+    newUsersArrObj[usersArr[i].key] = usersArr[i].value;
+  }
+  //write newUsersArrObj to json file as strings
+  //var jsonUserDatabase = JSON.stringify(newUsersArrObj);
+  fs.writeFile("users.json", JSON.stringify(newUsersArrObj), (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    };
+    console.log("JSON file has been created");
+  });
   alert("We are still working on attaching a json file to the user database. Here is your info: username: " + usersArr[0].password + ", password: " + usersArr[0].password + " & email: " + usersArr[0].emailAddress);
 };
