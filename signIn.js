5265ad3d3ea260a87b6063359efab52cfa41779c
Katie Taylor <katiemxtaylor@gmail.com>
2018-08-17 09:05:32 -0700

    day 4

--- signIn.js
+++ signIn.js
@@ -1,5 +1,5 @@
 var signedIn
-function signInCheck(){
+function signInCheck() {
   signedIn = false;
 
   //sign in test w/ info:
@@ -14,17 +14,27 @@ function signInCheck(){
   var username_entered = document.getElementById('usernameSignInBox').value;
   var password_entered = document.getElementById('passwordSignInBox').value;
 
-  //checks if the password and username entered match with each user in the array
+  //checks if the password and username entered match with each user in the database
   for (i in usersArr) {
     if (username_entered == usersArr[i].username && password_entered == usersArr[i].password) {
       signedIn = true;
-      alert("correct (Pretend we sent you to the home page)")
-      // need to send user to home screen
+      alert("correct");
+      //not sending user home for some reason, it works when placed outside of this function
+      window.location.replace("file:///C:/Users/Girls%20Who%20Code/Desktop/GWC-Chatr.github.io/home.html");
+      break
     }
   }
   //if the username and password don't match in a user object
   if (signedIn == false) {
-    alert("Your username/password is incorrect.")
+    alert("Your username/password is incorrect.");
   }
 
 }
+
+//failed taffy database
+// usersDatabase().each(function checkRecords(record,recordnumber) {
+//   if (username_entered == record["username"] && password_entered == record["password"]) {
+//     signedIn = true;
+//     window.location.replace("file:///C:/Users/Girls%20Who%20Code/Desktop/GWC-Chatr.github.io/home.html");
+//   }
+// )};
