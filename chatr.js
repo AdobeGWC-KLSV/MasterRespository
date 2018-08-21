d12a572d4eaef141becc00e51b05812a50b19997
Katie Taylor <katiemxtaylor@gmail.com>
2018-08-21 12:53:39 -0700

    KT

--- chatr.js
+++ chatr.js
@@ -1,21 +1,24 @@
-// var chat = [];
+//window.onload makes it so the initial things happen only after the page is actually loaded
 var post1Filled = false;
+window.onload = function () {
 //hiding the post boxes
-var post1 = document.getElementById("postBox1");
-// post1.style.visibility = "hidden";
-post1.hide();
-var post2 = document.getElementById("postBox2");
-// post2.style.display = "none";
+document.getElementById("postBox1").style.visibility = "hidden";
+document.getElementById("postBox2").style.visibility = "hidden";
+}
+
 function post() {
     var postTextEntered = document.getElementById("myText2").value;
     if(post1Filled == false) {
       document.getElementById("textSpace1").innerHTML = postTextEntered;
       post1Filled = true;
-
+      document.getElementById("noPosts").style.display = "none";
+      document.getElementById("postBox1").style.visibility = "visible";
       console.log("in if statement")
     }
     else {//need to add more else ifs and variables for more posts
       document.getElementById("textSpace2").innerHTML = postTextEntered;
+      document.getElementById("postBox1").style.visibility = "visible";
+      document.getElementById("postBox2").style.visibility = "visible";
     }
 
 
@@ -39,12 +42,22 @@ function moveIcon() {
 
 }
 
-function thumbsup() {
-  document.getElementById("downicon").style.display = "none";
-  document.getElementById("thumbicon").style.display = "block";
+function thumbsup1() {
+  document.getElementById("downicon1").style.display = "none";
+  document.getElementById("thumbicon1").style.display = "block";
 }
 
-function thumbsdown() {
-  document.getElementById("thumbicon").style.display ="none";
-  document.getElementById("downicon").style.display = "block";
+function thumbsdown1() {
+  document.getElementById("thumbicon1").style.display ="none";
+  document.getElementById("downicon1").style.display = "block";
+}
+
+function thumbsup2() {
+  document.getElementById("downicon2").style.display = "none";
+  document.getElementById("thumbicon2").style.display = "block";
+}
+
+function thumbsdown2() {
+  document.getElementById("thumbicon2").style.display ="none";
+  document.getElementById("downicon2").style.display = "block";
 }
